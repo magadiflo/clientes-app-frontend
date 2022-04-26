7aeef7364fac313ec1f8565edcf42810f3697b26
Martín <magadiflo@gmail.com>
2022-04-25 22:53:36 -0500

    Creando el componente cliente.component.ts y la interfaz cliente.interface.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ClienteComponent } from './clientes/cliente/cliente.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    ClienteComponent,
   ],
   imports: [
     BrowserModule,
